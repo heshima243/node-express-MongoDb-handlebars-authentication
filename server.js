be4532eeef811c01e98cd7e4ab33773dae3af007
heshima243 <heshimajulien.official@gmail.com>
2023-10-12 14:35:05 +0200

    improvement

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session({
 }));
 
 app.use(cors({
-  origin: ['https://julweb.netlify.app/'],
+  origin: ['https://julweb.netlify.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
 
